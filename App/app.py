5aa7686e81fa780741b8d4d6400014a017401dbc
Irene Martín Rodríguez <imartin@dtbird.com>
2025-04-29 16:30:55 +0200

    app.py

--- App/app.py
+++ App/app.py
@@ -2,12 +2,10 @@
 
 
 #Import libraries
-import os
-import sys
-sys.path.append(os.path.abspath('../src'))
-from import_images import create_image_dataset
-from image_preprocesser import preprocess_image, apply_preprocessing
-from extract_features import extract_features
+import streamlit as st
+from PIL import Image
+from src.image_preprocesser import preprocess_image, apply_preprocessing
+from src.extract_features import extract_features
 import tensorflow as tf
 from keras.applications import VGG16
 import matplotlib.pyplot as plt
@@ -15,7 +13,42 @@ import numpy as np
 from lightgbm import LGBMClassifier
 import joblib
 
-#Import model
-lgb = joblib.load("../App/lgbm.pkl")
 
-#Import image and preprocessing
+# === APP SETTINGS ===
+st.set_page_config(page_title="🦴 OsteoAI: Fracture Classifier", layout="centered")
+st.title("Automatically AI classificator of X-Ray Images")
+
+
+
+
+# === LOAD MODEL AND PRETRAINED CNN ===
+@st.cache_resource
+def load_model():
+    '''Function to load LBG model'''
+    model = joblib.load("lgbm.pkl")
+    return model
+
+@st.cache_resource
+def load_vgg16():
+    '''Function to load VGG16 network'''
+    base_model = VGG16(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
+
+    for layer in base_model.layers:
+        layer.trainable = False
+    for layer in base_model.layers[-8:]:
+        layer.trainable = True
+
+    return base_model
+
+
+
+# === LOAD MODELS ===
+model = load_model()
+vgg16 = load_vgg16()
+
+
+
+
+
+
+
